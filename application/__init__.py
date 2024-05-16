2ce1379139d0122a421633692428f2ad18058ebd
francisperes89 <francis.po@hotmail.com>
2024-05-16 22:26:19 +0100

    first commit

---
+++ application/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
+from config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+bootstrap = Bootstrap(app)
+login = LoginManager(app)
+login.login_view = 'login'
+
+from application import routes, models
